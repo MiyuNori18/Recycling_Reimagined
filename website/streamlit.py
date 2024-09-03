f63e20fed8837b8bae2750c3b3c246678764dd80
Miyuki Niyungeko <niyungekomiyuki@gmail.com>
2024-09-03 11:31:15 +0200

    Basic web page test 2

--- website/streamlit.py
+++ website/streamlit.py
@@ -7,6 +7,15 @@ This is a first website draft.
 
 '''
 
+st.markdown("# Welcome to My Awesome Website")
+
+# Add a subtitle
+st.markdown("## Test 2")
+
+# You can add more content here
+st.write("Test 3")
+
+
  # Set page tab display
 st.set_page_config(
    page_title="Recycling Reimagined",
@@ -14,34 +23,11 @@ st.set_page_config(
    layout="wide",
    initial_sidebar_state="expanded",
 )
-# Example local Docker container URL
-# url = 'http://api:8000'
-# Example localhost development URL
-url = 'http://localhost:8000'
-# url = os.getenv('API_URL')
 
-# App title and description
-st.header('Simple Image Uploader 📸')
+st.markdown("# Welcome to My Awesome Website")
 
-st.markdown("---")
-''''
-### Create a native Streamlit file upload input
-st.markdown("### Let's do a simple Invoice Extraction👇")
-img_file_buffer = st.file_uploader('Upload an Invoice')
+# Add a subtitle
+st.markdown("## Test 2")
 
-
-    with st.spinner("Wait for it..."):
-      ### Get bytes from the file buffer
-      img_bytes = img_file_buffer.getvalue()
-
-      ### Make request to  API (stream=True to stream response as bytes)
-      res = requests.post(url + "/upload_image", files={'img': img_bytes})
-
-      if res.status_code == 200:
-        ### Display the image returned by the API
-        st.image(res.content, caption="Invoice Extracted ☝️")
-      else:
-        st.markdown("**Oops**, something went wrong 😓 Please try again.")
-        print(res.status_code, res.content)
-
-        '''
+# You can add more content here
+st.write("Test 3")
