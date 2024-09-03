f2d2c565a0d49d5464e0687692f731db83555d15
Anton Wenemoser <@users.noreply.github.com>
2024-09-03 15:05:43 +0200

    streamlit working with image and annotation function added, added git ignore

--- website/streamlit.py
+++ website/streamlit.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 import requests
+import numpy as np
+
+#local
+from annotations import create_image
 
 # Set page tab display
 st.set_page_config(
@@ -14,8 +18,10 @@ url = 'http://127.0.0.1:8000'
 
 
 ### Create a native Streamlit file upload input
-st.markdown("### Let's do a simple face recognition 👇")
+st.markdown("### Let's do a simple image recognition 👇")
 img_file_buffer = st.file_uploader('Upload an image')
+
+
 
 if img_file_buffer is not None:
 
@@ -23,20 +29,28 @@ if img_file_buffer is not None:
 
   with col1:
     ### Display the image user uploaded
-    st.image(Image.open(img_file_buffer), caption="Here's the image you uploaded ☝️")
+    pil_image =Image.open(img_file_buffer)
+    np_image = np.array(pil_image)
+    st.image(pil_image, caption="Here's the image you uploaded ☝️")
 
-#   with col2:
-#     with st.spinner("Wait for it..."):
-#       ### Get bytes from the file buffer
-#       img_bytes = img_file_buffer.getvalue()
+  with col2:
+     with st.spinner("Wait for it..."):
+       ### Get bytes from the file buffer
+       img_bytes = img_file_buffer.getvalue()
 
-#       ### Make request to  API (stream=True to stream response as bytes)
-#       res = requests.post(url + "/predict_image", files={'img': img_bytes})
+       ### Make request to  API (stream=True to stream response as bytes)
+       res = requests.post(url + "/predict_image", files={'img': img_bytes})
 
-#       if res.status_code == 200:
-#         ### Display the image returned by the API
-#         st.write(res.json())
-#         #st.image(res.content, caption="Image returned from API ☝️")
-#       else:
-#         st.markdown("**Oops**, something went wrong 😓 Please try again.")
-#         print(res.status_code, res.content)
+       if res.status_code == 200:
+         ### Display the image returned by the API
+         st.write(res.json())
+
+         prediction = res.json()
+
+
+         image = create_image(np_image, prediction['boxes'])
+
+         st.image(image)
+       else:
+         st.markdown("**Oops**, something went wrong 😓 Please try again.")
+         print(res.status_code, res.content)
